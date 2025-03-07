b8c915bec450a045df34281fde4eb7388c72cb38
hcarolr <85520416+hcarolr@users.noreply.github.com>
2025-03-07 14:19:25 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+alert('Boas vindas ao nosso site!');
+
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+
+alert (mensagemDeErro);
+
+nome = prompt('Digite seu nome');
+idade = prompt('Digite sua idade');
+
+if (idade >= 18){
+    alert('Pode tirar a habilitação!');
+}
